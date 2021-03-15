58cca0526d470aeb1930e96a54e0f780884da800
auielol <78957067+auielol@users.noreply.github.com>
2021-03-15 20:53:28 +0800

    Add files via upload

--- TEST.c
+++ TEST.c
@@ -6,17 +6,60 @@ struct node
 {
     int data;
     struct node *next;
-}*head;
+};
 
+struct node *head = NULL;
+
+int getSelection(int select);
 void createList();
 void displayList();
+void sortList();
 
 int main()
 {
-	createList();
+	menu();
+}
+
+int menu()
+{
+	int select;
 	
-	printf("\nData in the list \n");
-    displayList();
+	printf("\n[1] CREATE LIST");
+	printf("\n[2] DISPLAY LIST");
+	printf("\n[3] EXIT\n");
+	
+	printf("\nSELECTION: ");
+	scanf("%d", &select);
+	
+	getSelection(select);
+}
+
+int getSelection(int select)
+{
+	switch (select)
+	{
+		case 1:
+			{
+				createList();
+				break;
+			}
+		case 2:
+			{
+				displayList();
+				break;
+			}
+		case 3:
+			{
+				return 0;
+			}
+		default:
+			{
+				printf("INVALID INPUT");
+				return menu();
+			}
+	}
+	
+	return menu();
 }
 
 void createList()
@@ -27,35 +70,70 @@ void createList()
     head = (struct node *)malloc(sizeof(struct node));
     
     time_t t;
-		srand((unsigned) time(&t));
-		
-        data = rand() %101;
+	srand((unsigned) time(&t));
+	
+	data = rand() %101;
 
-        head->data = data;
-        head->next = NULL;
+    head->data = data;
+    head->next = NULL;
 
-        temp = head;
+    temp = head;
 
-        for(i=2; i <= 10; i++)
+    for(i=2; i <= 10; i++)
+    {
+        newNode = (struct node *)malloc(sizeof(struct node));
+
+        if(newNode == NULL)
         {
-            newNode = (struct node *)malloc(sizeof(struct node));
-
-            if(newNode == NULL)
-            {
-                printf("Unable to allocate memory.");
-                break;
-            }
-            else
-            {
-                data = rand() %101;
-
-                newNode->data = data;
-                newNode->next = NULL;
-
-                temp->next = newNode;
-                temp = temp->next; 
-            }
+            printf("Unable to allocate memory.");
+            
+            break;
         }
+        else
+        {
+            data = rand() %101;
+
+            newNode->data = data;
+            newNode->next = NULL;
+
+            temp->next = newNode;
+            temp = temp->next; 
+        }
+    }
+    
+    sortList();
+}
+
+void sortList()
+{
+	struct node *current = head, *index = NULL;  
+    int temp;  
+          
+    if(head == NULL)
+	{  
+        return;  
+    }  
+    else
+	{  
+        while(current != NULL)
+		{  
+            index = current->next;  
+                  
+            while(index != NULL) 
+			{
+                if(current->data > index->data)
+				{  
+                    temp = current->data;  
+                    current->data = index->data;  
+                    index->data = temp;  
+                }  
+                index = index->next;  
+            }  
+            
+            current = current->next;  
+            
+        }      
+    }  
 }
 
 void displayList()
