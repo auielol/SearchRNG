9aa5c6bfb689769af40bd8fff8b7a83a8225813c
auielol <78957067+auielol@users.noreply.github.com>
2021-03-15 21:27:29 +0800

    Add files via upload

--- TEST.c
+++ TEST.c
@@ -10,13 +10,18 @@ struct node
 
 struct node *head = NULL;
 
+int menu();
 int getSelection(int select);
 void createList();
 void displayList();
-void sortList();
+void radixSort();
+int maxDig();
+int findDig();
 
 int main()
 {
+	printf("\n*-*-* SEARCH LIST *-*-*\n");
+	
 	menu();
 }
 
@@ -24,9 +29,11 @@ int menu()
 {
 	int select;
 	
-	printf("\n[1] CREATE LIST");
+	printf("\n[1] CREATE NEW LIST");
 	printf("\n[2] DISPLAY LIST");
-	printf("\n[3] EXIT\n");
+	printf("\n[3] BINARY SEARCH");
+	printf("\n[4] LINEAR SEARCH");
+	printf("\n[5] EXIT\n");
 	
 	printf("\nSELECTION: ");
 	scanf("%d", &select);
@@ -40,16 +47,35 @@ int getSelection(int select)
 	{
 		case 1:
 			{
+				system("cls");
+				
 				createList();
+				
+				printf("\nLIST CREATION SUCCESSFUL\n");
+				
 				break;
 			}
 		case 2:
 			{
+				system("cls");
+				
 				displayList();
 				break;
 			}
 		case 3:
 			{
+				
+			}
+		case 4:
+			{
+				
+			}
+		case 5:
+			{
+				system("cls");
+				
+				printf("\nEXITING PROGRAM\n");
+				
 				return 0;
 			}
 		default:
@@ -65,7 +91,7 @@ int getSelection(int select)
 void createList()
 {
 	struct node *newNode, *temp;
-    int data, i;
+    int data, i, count = 0;
     
     head = (struct node *)malloc(sizeof(struct node));
     
@@ -101,56 +127,120 @@ void createList()
         }
     }
     
-    sortList();
+    radixSort();
 }
 
-void sortList()
+void radixSort()
 {
-	struct node *current = head, *index = NULL;  
-    int temp;  
-          
-    if(head == NULL)
-	{  
-        return;  
-    }  
-    else
-	{  
-        while(current != NULL)
-		{  
-            index = current->next;  
-                  
-            while(index != NULL) 
-			{
-                if(current->data > index->data)
-				{  
-                    temp = current->data;  
-                    current->data = index->data;  
-                    index->data = temp;  
-                }  
-                index = index->next;  
-            }  
+    int i, k, digit, leastSig, mostSig;
+      
+    struct node *rear[10], *front[10], *p;
+      
+    leastSig = 1;
+      
+    mostSig = maxDig(head);
+      
+    for(k = leastSig; k <= mostSig; k++)
+    {
+    for(i = 0; i <= 9; i++)
+    {
+        rear[i] = NULL;
+        front[i] = NULL;
+    }
+        
+    for(p = head; p != NULL; p = p->next)
+    {
+        digit = findDig(p->data, k);
             
-            current = current->next;  
+        if(front[digit] == NULL) 
+        {
+            front[digit] = p;
+        }
+        else
+        {
+            rear[digit]->next = p;
+        }
+            rear[digit] = p;
+        }
             
-        }      
-    }  
+        i = 0;
+            
+        while(front[i] == NULL)
+        {
+            i++;
+        }
+            
+        head = front[i];
+            
+        while(i < 9) 
+        {
+            if(rear[i + 1] != NULL)
+            {
+                rear[i]->next = front[i + 1];
+            }
+            else
+            {
+                rear[i + 1] = rear[i];
+            }
+            i++;
+        }
+        rear[9]->next = NULL;
+    }
+}
+ 
+int maxDig()
+{
+	int temp = 0, digit = 0;
+	
+    struct node *p = head;
+      
+    while(p != NULL)
+    {
+        if(p ->data > temp)
+        {
+            temp = p->data;
+        }
+        p = p->next ;
+    }
+    while(temp != 0)
+    {
+        digit++;
+        temp = temp / 10;
+    }
+    return(digit);
+}
+ 
+int findDig(int number, int k)
+{
+    int term, i;
+    
+    for(i = 1; i <= k; i++)
+    {
+        term = number % 10;
+        number = number / 10;
+    }
+    return(term);
 }
 
 void displayList()
 {
+	int count = 1;
+	
 	struct node *temp;
 	
 	if(head == NULL)
     {
-        printf("List is empty.");
+        printf("\nList is empty.\n");
     }
     else
     {
         temp = head;
         while(temp != NULL)
         {
-            printf("Data = %d\n", temp->data);
+            printf("Data %d = %d\n", count, temp->data);
             temp = temp->next;
+            
+            count++;
         }
     }
 }
